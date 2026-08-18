2a0e70173d64031bdb574eea04323c79eeb0a1c7
Bachdangdbtsh <bachjuste1511@gmail.com>
2026-08-18 09:43:20 +0700

    Tao folder examQuestion va code phan repository

---
+++ backend/src/modules/examQuestion/examQuestion_repository.ts
@@ -0,0 +1,57 @@
+import type { ZodNumberFormat } from "zod";
+import { prisma } from "../../config/prisma.js"
+import { ExamRepository } from "./exam_repository.js"
+
+export class ExamQuestionRepository {
+    async findByID(id: string) {
+        return prisma.examQuestion.findUnique({
+            where: {id},
+            include: {
+                question: { include: { options: true } } ,
+            }
+        });
+    }
+
+    async listByExam(examID: string) {
+        return prisma.examQuestion.findMany({
+            where: {examID},
+            include: { 
+                question: { include: { options: true } } 
+            },
+            orderBy: { order: "asc" },
+        });
+    }
+
+    async findExamAndQuestion(examID: string, questionID: string) {
+        return prisma.examQuestion.findUnique( {
+            where: {
+                examID_questionID: {examID, questionID}
+            },
+        });
+    }
+
+    async createExamQuestion(data: {
+        examID: string,
+        questionID: string,
+        order: number,
+        points: number
+    }) {
+        return prisma.examQuestion.create({data});
+    }
+
+    async updateExamQuestion(id: string, data: {
+        examID: string,
+        questionID: string,
+        order: number,
+        points: number
+    }) {
+        return prisma.examQuestion.update({
+            where: {id},
+            data
+        });
+    }
+
+    async deleteExamQuestion(id: string) {
+        return prisma.examQuestion.delete({ where: {id} });
+    }
+}
---
+++ backend/src/modules/examQuestion/exam_repository.ts
@@ -0,0 +1,53 @@
+import { includes } from "zod"
+import { prisma } from "../../config/prisma.js"
+
+export class ExamRepository {
+    async findByID(id: string) {
+        return prisma.exam.findUnique({    
+            where: {id},
+            include: {
+                examQuestions: {
+                    include: { question: { include: { options: true } } },
+                    orderBy: { order: "asc" },
+                }
+            }}
+        )
+    }
+
+    async listByOwner(ownerID: string) {
+        return prisma.exam.findMany({
+            where: {ownerID},
+            include: {
+                examQuestions: {
+                    include: { question: true },
+                    orderBy: { order: "asc" }
+                }
+            }
+        })
+    }
+
+    async createExam(data: {
+        name: string,
+        description: string,
+        ownerID: string
+    }) {
+        return prisma.exam.create({data});
+    }
+
+    async updateExam(id: string, data: {
+        name: string, 
+        description: string
+    }) {
+        return prisma.exam.update({
+            where: {id},
+            data,
+        });
+    }
+
+    async deleteExam(id: string) {
+        return prisma.exam.delete({
+            where: {id}
+        });
+    }
+    
+}
